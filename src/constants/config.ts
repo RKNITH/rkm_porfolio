6276d2479094258ee319e8517871baae104e94b6
RKNITH <raviranjankofficial@gmail.com>
2024-05-20 02:03:11 +0530

    first commit

---
+++ src/constants/config.ts
@@ -0,0 +1,90 @@
+type TSection = {
+  p: string;
+  h2: string;
+  content?: string;
+};
+
+type TConfig = {
+  html: {
+    title: string;
+    fullName: string;
+    email: string;
+  };
+  hero: {
+    name: string;
+    p: string[];
+  };
+  contact: {
+    form: {
+      name: {
+        span: string;
+        placeholder: string;
+      };
+      email: {
+        span: string;
+        placeholder: string;
+      };
+      message: {
+        span: string;
+        placeholder: string;
+      };
+    };
+  } & TSection;
+  sections: {
+    about: Required<TSection>;
+    experience: TSection;
+    feedbacks: TSection;
+    works: Required<TSection>;
+  };
+};
+
+export const config: TConfig = {
+  html: {
+    title: "Raviranjan",
+    fullName: "Raviranjan",
+    email: "rr8422617@gmail.com",
+  },
+  hero: {
+    name: "Raviranjan",
+    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      name: {
+        span: "Your Name",
+        placeholder: "What's your name?",
+      },
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+      content: `I am a dedicated full stack web developer with expertise in both front-end and back-end technologies, including HTML, CSS, JavaScript, React, Angular, Node.js, Express, MongoDB, and MySQL. Additionally, I have in-depth knowledge of Python, using it for back-end development, data analysis, and automation with frameworks like Django and Flask and libraries such as Pandas, NumPy, and Matplotlib. Passionate about continuous learning and industry trends, I excel in creating intuitive, responsive user interfaces and robust, scalable web applications, always aiming to deliver high-quality work that meets and exceeds client expectations.`,
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    feedbacks: {
+      p: "What others say",
+      h2: "Testimonials.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
